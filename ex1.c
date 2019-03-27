420480cca45a44d3acb7e8474ad520f7d09d7869
arshadumrethi <arshad.umrethi@gmail.com>
2019-03-27 20:02:29 +0530

    Add new files

---
+++ ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* This is a comment. */
+
+int main(int argc, char *argv[])
+{
+    int distance = 100 * 100;
+    int more = distance + 111;
+    // this is also a comment
+
+printf("You are %d miles away.\n", more);
+
+    return 0;
+}
